9fc0a156c8f6313ff2ff601309ba7d2f3ef6f2ba
qwertyLEEJK <dpthree00@yu.ac.kr>
2025-06-15 18:23:17 +0900

    feat 지도띄우기

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,39 @@
+// import 'package:flutter/material.dart';
+// import 'screens/campus_map_screen.dart';
+// import 'components/bottom_Navbar.dart';
+// import 'screens/sensor_test.dart';
+// import 'screens/kakaoMapScreen.dart';
+// // 추가!
+// import 'screens/room_list_screen.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: '캠퍼스 지도',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(useMaterial3: true),
+//       home: Scaffold(
+//         body: RoomListScreen(), // <- 여기를 RoomListScreen()으로!
+//         // body: SensorTest(),
+//         // body: Kakaomapscreen(),
+//         bottomNavigationBar: BottomNavBar(
+//           navigating: false,
+//           onStopNavigation: () {},
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'screens/campus_map_screen.dart';
 import 'components/bottom_Navbar.dart';
-import 'screens/sensor_test.dart';
-import 'screens/kakaoMapScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,13 +49,40 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: Scaffold(
-        // body: SensorTest(),
-        body: Kakaomapscreen(),
-        bottomNavigationBar: BottomNavBar(
-          navigating: false,
-          onStopNavigation: () {},
+        appBar: AppBar(title: const Text('캠퍼스 지도')),
+        body: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return InteractiveViewer(
+                panEnabled: true,
+                minScale: 1,
+                maxScale: 5,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      minHeight: constraints.maxHeight,
+                    ),
+                    child: Image.asset(
+                      'lib/3map.png',
+                      fit: BoxFit.fitHeight, // 화면 세로에 맞춤
+                      height: constraints.maxHeight,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        bottomNavigationBar: SafeArea(
+          child: BottomNavBar(
+            navigating: false,
+            onStopNavigation: () {},
+          ),
         ),
       ),
     );
   }
 }
+
+
--- lib/screens/kakaoMapScreen.dart
+++ lib/screens/kakaoMapScreen.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
-class Kakaomapscreen extends StatelessWidget  {
-  const Kakaomapscreen({super.key});
+// class Kakaomapscreen extends StatelessWidget  {
+//   const Kakaomapscreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Kakao Map")),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(
-          url: Uri.parse("https://map.kakao.com"),
-          ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Kakao Map")),
+//       body: InAppWebView(
+//         initialUrlRequest: URLRequest(
+//           url: Uri.parse("https://map.kakao.com"),
+//           ),
+//       ),
+//     );
+//   }
+// }
